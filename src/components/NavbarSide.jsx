d4998be0d47c349aeae90448ad44cb9f465ea207
Hari-ghm <hmadhav2006@gmail.com>
2024-11-05 17:26:47 +0530

    photo error fixed!

--- src/components/NavbarSide.jsx
+++ src/components/NavbarSide.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NavbarSide = () => {
   
   return (
-    <div className="fixed top-1/4  ml-10 pl-5 min-h-96 max-w-16 bg-red-600 text-white p-8 rounded-full shadow-lg z-30 block gap-8">
+    <div className="fixed top-1/4  ml-10 pl-5 min-h-96 max-w-16 bg-red-100 text-white p-8 rounded-full shadow-lg z-30 block gap-8">
       <div className="text-white">hijnj</div>
       <div className="text-white">hijnj</div>
     </div>
